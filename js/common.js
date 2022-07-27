ad9cf3cca23dd908101c7a4a95e4b8236fcddd53
TarasDutchak <tarasdutchak90@gmail.com>
2022-07-27 12:18:35 +0300

    blog

--- js/common.js
+++ js/common.js
@@ -217,5 +217,21 @@
 	});
 
 
+	// page progress bar
+	if ($('.progress-bar').length) {
+		window.onscroll = function () {
+			myFunction()
+		};
+
+		function myFunction() {
+			var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+			var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+			var scrolled = (winScroll / height) * 100;
+			document.getElementById("myBar").style.width = scrolled + "%";
+		}
+	}
+
+
+
 
 }(jQuery));
