3a048f463e1d12d760498cb2f657fd0b21b7432c
Eugene Mazuryk <mazuryk.e@ukr.net>
2023-04-28 10:51:46 +0300

    Update common.js

--- js/common.js
+++ js/common.js
@@ -537,21 +537,58 @@
 
 	
 
+	/*** Post content navigation ***/
+	var headers = document.querySelectorAll('.postcontent h2');
+	if (headers.length !== 0) {
+		for (var i = 0; i < headers.length; i++) {
+			headers[i].setAttribute('id', i + 1);
+		}
+
+		var list = document.querySelector('.postnav');
+		var ul = document.createElement('ul');
+
+		for (var i = 0; i < headers.length; i++) {
+			var text = headers[i].textContent;
+			var id = headers[i].getAttribute('id');
+			var listItem = document.createElement('li');
+			var link = document.createElement('a');
+
+			link.textContent = text;
+			link.setAttribute('href', '#' + id);
+			listItem.appendChild(link);
+			ul.appendChild(listItem);
+		}
+
+		list.appendChild(ul);
+
+		// -----------.
+		$(".postnav ul").on("click", "a", function (event) {
+			console.log('sdsdsd')
+			event.preventDefault();
+			var id = $(this).attr('href'),
+				top = $(id).offset().top;
+			$('body,html').animate({scrollTop: top - 105}, 1500);
+		});
+
+		const links = document.querySelectorAll('.postnav li a');
+		window.addEventListener('scroll', () => {
+			const anchors = document.querySelectorAll('.postmiansectbody__post [id]');
+			anchors.forEach(anchor => {
+				if (anchor.closest('.postmiansectbody__post')) {
+					const anchorTop = anchor.getBoundingClientRect().top + window.pageYOffset;
+					if (window.pageYOffset >= anchorTop - 110) {
+						links.forEach(link => {
+							const correspondingLink = document.querySelector(`.postnav li a[href="#${anchor.id}"]`);
+							if (correspondingLink) {
+								link.classList.remove('active');
+								correspondingLink.classList.add('active');
+							}
+						});
+					}
+				}
+			});
+		});
+	}
 	
-
-
-
-
-
-
-
-
-
-	
-
-
-
-
-
 
 }(jQuery));
